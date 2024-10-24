5f985ea28df4acb68079175245a57083274d767f
ujjwalsharmaIITB <142598228+ujjwalsharmaIITB@users.noreply.github.com>
2024-10-24 10:16:12 +0000

    Added System and Human prompt templates.

---
+++ code/get_prompt_templates.py
@@ -0,0 +1,55 @@
+import os
+
+os.environ["HF_HOME"] = "/home/ioeuser/chat/chat-tutorial/hf_cache"
+
+
+
+from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+
+
+
+
+from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+
+
+movies_review_system_message_template_str = """Your job is to use movie reviews to answer questions about the movies.
+Use the following context to answer questions.
+Be as detailed as possible, but don't make up any information that's not from the context.
+If you don't know an answer, say you don't know.
+
+context = {context}
+"""
+
+
+movies_review_human_message_template_str = """question = {question}"""
+
+
+
+
+movies_review_system_prompt = SystemMessagePromptTemplate(
+    prompt = PromptTemplate(
+        input_variables = ["context"],
+        template = movies_review_system_message_template_str
+    )
+)
+
+
+
+
+
+movies_review_human_prompt = HumanMessagePromptTemplate(
+    prompt = PromptTemplate(
+        input_variables = ["question"],
+        template = movies_review_human_message_template_str
+    )
+)
+
+
+messages = [movies_review_system_prompt, movies_review_human_prompt]
+
+
+movies_review_final_prompt = ChatPromptTemplate(
+    input_variables = ["context", "question"],
+    messages = messages
+)
+
