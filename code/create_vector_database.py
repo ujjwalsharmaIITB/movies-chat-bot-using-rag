ab73b05ef6e8ade534f1b2ee6ab5b3d911091335
ujjwalsharmaIITB <142598228+ujjwalsharmaIITB@users.noreply.github.com>
2024-10-24 10:17:21 +0000

    Added code to create vectorDB from csv using Chroma

---
+++ code/create_vector_database.py
@@ -0,0 +1,36 @@
+import os
+
+os.environ["HF_HOME"] = "/home/ioeuser/chat/chat-tutorial/hf_cache"
+
+
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import CSVLoader
+from langchain_community.vectorstores import Chroma
+
+# Define the model name
+
+model_name = "mixedbread-ai/mxbai-embed-large-v1"
+
+
+hf_embeddings = HuggingFaceEmbeddings(model_name=model_name)
+
+movies_review_dataset_path = "/home/ioeuser/chat/chat-tutorial/movies_data/IMDB-Movie-Dataset(2023-1951).csv"
+
+chroma_data_path = "chroma_data"
+
+loader = CSVLoader(file_path=movies_review_dataset_path, source_column="overview")
+
+movies_reviews = loader.load()
+
+print("Starting")
+movies_review_vector_database = Chroma.from_documents(
+    movies_reviews , hf_embeddings, persist_directory= chroma_data_path
+)
+
+print("Done")
+
+
+
+
+
+
